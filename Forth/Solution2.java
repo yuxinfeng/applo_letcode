869a6c342c3d408ce352b937e5e328d4f81bd07f
yuxinfeng <yuxinfeng@baidu.com>
2017-04-18 11:52:19 +0800

    update: 更新Fourth 的一种解题方法

--- Forth/Solution2.java
+++ Forth/Solution2.java
@@ -7,6 +7,21 @@ public class Solution2 {
         return a;
     }
 
+
+    /* *
+     * p,j两个指针遍历字符串
+     *
+     * */
+    public static int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+            }
+        }
+    }
+
     public static void main(String []args) {
         Solution2.strStr("aabccdad", "cd");
     }
